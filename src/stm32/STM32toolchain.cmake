ea7cbfd326db6c9b7af4d685461241c6d111cb52
Paolo Jovon <paolo.jovon@gmail.com>
2019-05-31 22:02:29 +0100

    Fixed the wrong CN_FLASH_BOOTLOADER_SIZE being set by STM32toolchain.cmake
    In any case there should be a size check vs. CN_FLASH_BOOTLOADER_SIZE somewhere
    in the linker script to make sure the bootloader actually fits!

--- src/stm32/STM32toolchain.cmake
+++ src/stm32/STM32toolchain.cmake
@@ -41,10 +41,11 @@ string(REPLACE ";" " " CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS_LIST}")
 
 # #define core macros required to build CANnuccia
 # FIXME: High-density (> 128kB) devices have 2kB pages, not 1kB
+# FIXME IMPORTANT: Make sure the bootloader actually fits in CN_FLASH_BOOTLOADER_SIZE!!
 # FIXME: This should likely be moved out of the toolchain file to somewhere better!
 add_definitions(
     -DCN_FLASH_PAGE_SIZE=0x400u # 1kB pages
     -DCN_FLASH_PAGE_MASK=0xFFFFFC00u
-    -DCN_FLASH_BOOTLOADER_SIZE=0x2000u # 4kB reserved to CANnuccia
+    -DCN_FLASH_BOOTLOADER_SIZE=0x1000u # 4kB reserved to CANnuccia
     -DCN_PLATFORM_IS_STM32=1
 )

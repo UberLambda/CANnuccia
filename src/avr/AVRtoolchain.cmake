3de75b9eb16d63d08cc9d056f582cf5bb90a45a4
Paolo Jovon <paolo.jovon@gmail.com>
2019-06-16 16:44:09 +0100

    Fixed AVRtoolchain.cmake not setting the right address for .text
    On AVR - unlike STM32 - bootloaders are at the end of flash!

--- src/avr/AVRtoolchain.cmake
+++ src/avr/AVRtoolchain.cmake
@@ -12,7 +12,8 @@ set(AVR_PART "atmega328p" CACHE STRING "The AVR microcontroller's part name")
 # 4kB bootloader is the maximum possible for ATMega328p (BOOTSZ=00).
 # With BOOTSZ=00, the whole NRWW section is occupied by the bootloader.
 # TODO: May differ for other AVR chips!
-set(AVR_BOOTLOADER_SIZE 4096 CACHE STRING "The size allocated to the bootloader section (via BOOTSZ)")
+set(AVR_FLASH_SIZE 32768 CACHE STRING "The total size of program flash, in bytes")
+set(AVR_BOOTLOADER_SIZE 4096 CACHE STRING "The size allocated to the bootloader section (via BOOTSZ), in bytes")
 
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR avr)
@@ -31,9 +32,19 @@ set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG}")
 set(CMAKE_C_FLAGS_RELEASE "-Os -mmcu=${AVR_PART} -flto -fstrict-volatile-bitfields")
 set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE}")
 
+
+# Bootloaders on AVR are at the end of flash
+math(EXPR BOOTLOADER_START_ADDR
+    "${AVR_FLASH_SIZE} - ${AVR_BOOTLOADER_SIZE}"
+    OUTPUT_FORMAT HEXADECIMAL
+)
+if((BOOTLOADER_START_ADDR LESS 0) OR (BOOTLOADER_START_ADDR GREATER_EQUAL ${AVR_FLASH_SIZE}))
+    message(FATAL_ERROR "Calculated bootloader start address out of range!")
+endif()
+
 set(CMAKE_EXE_LINKER_FLAGS_LIST
     -flto
-    -Wl,--section-start=.text=${AVR_BOOTLOADER_SIZE} # Relocate bootloader code
+    -Wl,--section-start=.text=${BOOTLOADER_START_ADDR} # Relocate bootloader code
 )
 string(REPLACE ";" " " CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS_LIST}")
 
